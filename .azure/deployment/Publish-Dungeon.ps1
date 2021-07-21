8b7d925f6d862cc0b7af22eeb3ed09ab5bc2abd4
Jeff Roy <jeffroy413@comcast.net>
2021-07-21 08:20:52 -0400

    working zip deploy with az webapp

--- .azure/deployment/Publish-Dungeon.ps1
+++ .azure/deployment/Publish-Dungeon.ps1
@@ -5,16 +5,20 @@ param(
     [string]$Location = $(throw "Location is required")
 )
 <#
-./Publish-Dungeon -AppName RoyDungeon -ResourceGroup DungeonResourceGroup -Location eastus -Plan dungeonPlan
+./Publish-Dungeon -AppName RoyDungeon -ResourceGroup DungeonResourceGroup -Location eastus -Plan DungeonPlan
 ./Publish-Dungeon -AppName RoyDungeon -ResourceGroup DungeonResourceGroup -Location eastus
 #>
 
 $resourceExists = az group exists --name $ResourceGroup
 if ($resourceExists -eq $false) {
     Write-Output "$ResourceGroup does not exist, creating"
-    az group create --name $ResourceGroup --location $Location --output table
+    az group create --name $ResourceGroup --location $Location
+
     Write-Output "Creating app service plan $Plan for $ResourceGroup"
-    az appservice plan create --name $Plan --resource-group $ResourceGroup --sku FREE --output table
+    az appservice plan create --name $Plan --resource-group $ResourceGroup --sku FREE
+
+    Write-Output "Creating webapp $AppName for $ResourceGroup"
+    az webapp create --resource-group $ResourceGroup --plan $Plan --name $AppName --runtime "DOTNETCORE:3.1"
 }
 else {
     Write-Output "$ResourceGroup already exists"
@@ -28,54 +32,11 @@ if (Test-Path -Path $deployFolder)
     Remove-Item $deployFolder -Recurse
 }
 
-Write-Output "Creating $AppName"
-New-AzWebApp -ResourceGroupName $ResourceGroup -AppServicePlan $Plan -Name $AppName 
-#New-AzWebApp creates a git remote named azure
 
-#$deplomentConfig = az webapp create --resource-group $ResourceGroup --plan $Plan --name $AppName --runtime "DOTNETCORE:3.1" --deployment-local-git
+dotnet publish ../../Dungeon.Web/Dungeon.Web.csproj --configuration Release --output ./publish/www
 
-$webApp
-#Write-Output "git app url: $($deplomentConfig.deploymentLocalGitUrl)"
+Compress-Archive -Path ./publish/www/* -DestinationPath "publish/$AppName.zip"
 
-#dotnet publish ../../Dungeon.Web/Dungeon.Web.csproj --configuration Release --output ./publish
-#dotnet build ../../Dungeon.Web/Dungeon.Web.csproj /nologo /p:PublishProfile=Release /p:PackageLocation="C:\Some\Path\package" /p:OutDir="C:\Some\Path\out" /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /maxcpucount:1 /p:platform="Any CPU" /p:configuration="Release" /p:DesktopBuildPackageLocation="C:\Some\Path\package\package.zip"
-#dotnet build ../../Dungeon.Web/Dungeon.Web.csproj /nologo /p:PublishProfile=Release /p:PackageLocation="./publish" /p:OutDir="./out" /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PackageAsSingleFile=true /maxcpucount:1 /p:platform="Any CPU" /p:configuration="Release" /p:DesktopBuildPackageLocation="./publish/package.zip"
-
-# Set-Location publish
-
-# Write-Host "Creating $AppName as an Azure webapp"
-
-# #https://github.com/Azure/azure-cli/issues/15832
-# ##az webapp up --dryrun --name $AppName --resource-group $ResourceGroup --runtime '"DOTNETCORE|3.1"' --sku F1
-# #az webapp create --name $AppName --plan $Plan --resource-group $ResourceGroup --runtime '"DOTNETCORE|3.1"'
-# #az webapp create --name $AppName --resource-group $ResourceGroup --plan $Plan --runtime '"DOTNETCORE|3.1"' --deployment-local-git
-
-# #az webapp up --name $AppName --resource-group $ResourceGroup --plan $Plan --runtime "DOTNETCORE:3.1"
-# #https://github.com/MicrosoftDocs/azure-docs/issues/43633
-
-# az webapp up --name $AppName --resource-group $ResourceGroup --plan $Plan --html
-
-# #https://docs.microsoft.com/en-us/azure/app-service/scripts/cli-deploy-local-git
-
-# #https://github.com/h11bear/Dungeon.git
-
-# Set-Location ..
-
-# #az group delete --resource-group DungeonResourceGroup
-# #az webapp up --name RoyDungeon --resource-group DungeonResourceGroup --plan dungeonPlan --runtime "DOTNETCORE:3.1" --html
-# #az webapp up --name RoyDungeon --resource-group DungeonResourceGroup --plan dungeonPlan --html
-
-# $PropertiesObject = @{
-#     scmType = "LocalGit";
-# }
-
-# Set-AzResource -PropertyObject $PropertiesObject -ResourceGroupName roydungeon1 `
-# -ResourceType Microsoft.Web/sites/config -ResourceName RoyDungeon1/web `
-# -ApiVersion 2015-08-01 -Force
-
-# git remote add azure https://roydungeon1.scm.azurewebsites.net:443/RoyDungeon1.git
-# git push azure master
+az webapp deployment source config-zip --resource-group DungeonResourceGroup --name RoyDungeon --src "publish/$AppName.zip"
 
 #Remove-AzResourceGroup -Name DungeonResourceGroup
-#Remove-AzWebApp -ResourceGroupName DungeonResourceGroup -Name RoyDungeon -Force
-#New-AzWebApp -ResourceGroupName DungeonResourceGroup -AppServicePlan dungeonPlan -Name RoyDungeon 
