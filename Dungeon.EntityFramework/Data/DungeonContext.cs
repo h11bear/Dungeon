5965b91677433a5bb1cb06be5bbe18d9f6ce8db4
Jeff Roy <jeffroy413@comcast.net>
2022-11-30 05:45:17 -0500

    trying out entity framework

---
+++ Dungeon.EntityFramework/Data/DungeonContext.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Dungeon.Logic.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace Dungeon.EntityFramwork.Data 
+{
+    public class DungeonContext: DbContext
+    {
+        protected IConfiguration Configuration { get; }
+        public DungeonContext(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        public DbSet<RoomCatalog>? RoomCatalogs { get; set; }
+        public DbSet<Room>? Rooms { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DungeonDbConnectionString"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.ApplyConfiguration(new RoomEntityConfiguration());
+
+            // https://stackoverflow.com/questions/46497733/using-singular-table-names-with-ef-core-2
+            // Use the entity name instead of the Context.DbSet<T> name
+            // refs https://learn.microsoft.com/en-us/ef/core/modeling/entity-types?tabs=fluent-api#table-name
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                // 
+                if (entityType.ClrType.Name != "RoomExit")
+                {
+                    modelBuilder.Entity(entityType.ClrType).ToTable(entityType.ClrType.Name);
+                }
+            }            
+        }
+    }
+
+}
